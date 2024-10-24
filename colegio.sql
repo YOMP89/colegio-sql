2c8b73522ee7ae371d55d7578f068bbef3ab9ff9
Omar Parada <omarparada89@outlook.com>
2024-10-23 20:22:47 -0500

    Creación de la tabla de 'estudiantes'

--- colegio.sql
+++ colegio.sql
@@ -1,2 +1,17 @@
 -- Creación de la base de datos
 CREATE DATABASE Colegio;
+
+
+-- usar base de datos de colegio
+USE Colegio;
+
+-- Creación de la tabla de datos del coelgio
+
+CREATE TABLE Estudiantes (
+    ID INT PRIMAR KEY AUTO:INCREMENT,
+    Nombre VARCHAR(50),
+    Apellido VARCHAR(60),
+    Fecha_Nacimieto DATE,
+    Género VARCHAR(10)
+);
+
